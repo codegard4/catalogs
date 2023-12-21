671510107f29430f39bdfa9c4f8cf9bcec6261de
ColeOdegard1 <149621868+ColeOdegard1@users.noreply.github.com>
2023-12-21 11:35:20 -1000

    Add files via upload

--- plots/catalog_plots.py
+++ plots/catalog_plots.py
@@ -1,83 +1,24 @@
-# import math
-# import pandas as pd
-# import math
-# import pickle
-# import plotly as plt
-# import plotly.express as px
-# import numpy as np
-# from datetime import datetime
-# from astropy import coordinates as coord
-# from astropy import units as u
-# from astropy.time import Time
-# from tqdm import tqdm
-# from astropy.coordinates import SkyCoord
-# from datetime import datetime
-# from astropy.time import Time
-
 import math
-from datetime import datetime
 import pickle
-
 import pandas as pd
 import plotly as plt
 import plotly.express as px
 import numpy as np
+from datetime import datetime
 from tqdm import tqdm
-
-
-def createSAOBins():
-    sao_num,dec_rad_2000,ra_rad_2000 = [],[],[]
-    with open('../Plots/sao.dat', 'r') as f:
-        lines = f.readlines()
-        for line in tqdm(lines): 
-            line = str.encode(line)
-            sao_num.append(int(line[0:6].decode()))
-            ra_rad_2000.append(float(line[183:193].decode()))
-            dec_rad_2000.append(float(line[193:204].decode()))
-    catalog = pd.DataFrame(list(zip(sao_num,dec_rad_2000,ra_rad_2000)), columns = ['sao_num','dec_rad_2000','ra_rad_2000'])
-    xarr = np.array(catalog.iloc[:,2]) #RA
-    yarr = np.array(catalog.iloc[:,1]) #DEC
-    print(max(xarr)*180/math.pi)
-    print(max(yarr)*180/math.pi)
-    print(max(yarr)*90/math.pi)
-    a = np.zeros(shape=(180, 45))
-    for i in tqdm(range(len(xarr))):
-        # rastar = int((xarr[i] * 180)/math.pi)
-        # decstar = int((yarr[i] * 180)/math.pi)
-        
-        rastar = int(xarr[i] * 90/math.pi)
-        decstar = int(yarr[i] * 90/math.pi)
-        a[rastar][decstar] = a[rastar][decstar]+1
-        if(decstar > 45):
-            print(decstar)
-    with open("../Plots/sao_bins", "wb") as f:
-            pickle.dump(a, f)
-          
-        
-def getSAOBins():
-    try:
-        with open("../Plots/sao_bins", "rb") as f:
-            unpickled_array = pickle.load(f)
-        return unpickled_array
-    except Exception as e: 
-        print(e)
-        bins_sao = createSAOBins()
-        return bins_sao
-    
-def plotSAO():
-    a = getSAOBins()
-    b = [list(x) for x in zip(*a)]
-    ras, decs = scales([360,0],[0,90],180,45)
-    fig = px.imshow(b,
-            labels=dict(x="Right Ascension", y="Declination"),
-            y = decs,
-            x = ras,
-            title = "sao2000 Sky coverage"
-           )
-    fig.show()
-    plt.offline.plot(fig, filename='../Plots/sao2000.html')
+import os
 
 def scales(ra_range, dec_range, rabins, decbins):
+    """
+    Generates scales for RA and Declination based on specified ranges and number of bins.
+    Args:
+        ra_range (List): Range of RA values.
+        dec_range (List): Range of Declination values.
+        rabins (int): Number of RA bins.
+        decbins (int): Number of Declination bins.
+    Returns:
+        Tuple: Lists of RA and Declination scales.
+    """
     ra = np.arange(ra_range[1],ra_range[0],-(ra_range[1] - ra_range[0]) / rabins)
     dec = np.arange(dec_range[1],dec_range[0],-(dec_range[1] - dec_range[0]) / decbins)
     decs = []
@@ -86,68 +27,233 @@ def scales(ra_range, dec_range, rabins, decbins):
         decs.append(str(int(i)))
     for i in ra:
         ras.append(str(int(i)))
-    # print(ras, decs)
     return ras, decs
 
-def createUCACBins():
-    ra_bins = 360
-    dec_bins = 180
-    assert(ra_bins <= 1440)
-    assert(dec_bins <=900)
-    rafac = int(1440/ra_bins)
-    decfac = int(900/dec_bins)
-    bins = np.arange(ra_bins * dec_bins).reshape(ra_bins, dec_bins)
-    with open(f'..ucac4/u4i/u4index.asc', 'r') as f:
-        for line in f:
-            x = line.split(" ")
-            x = [i for i in x if i]
-            count = int(x[1])
-            dec = int(x[2])
-            ra = int(x[3])
-            for i in range(0,dec_bins):
-                for j in range(0,ra_bins):
-                    if(dec // decfac == i):
-                        if(ra // rafac == j):
-                            bins[j][i] = bins[j][i] + count
-    with open("../Plots/ucac_bins", "wb") as f:
-        pickle.dump(bins, f)
-    return bins
-
+def getSAOBins():
+    """
+    Retrieves SAO bins from the pickle file. 
+    Returns:
+        Numpy array: SAO bins.
+    """
+    try:
+        with open(f"{os.getcwd()}/sao_bins", "rb") as f:
+            unpickled_array = pickle.load(f)
+        return unpickled_array
+    except Exception as e:
+        print(e)
+    
+def plotSAO():
+    """
+    Plots SAO Sky coverage using the SAO bins.
+    Returns:
+        None
+    """
+    a = getSAOBins()
+    b = [list(x) for x in zip(*a)]
+    ras, decs = scales([360, 0],[-90, 90],360, 180)
+    fig = px.imshow(
+        b,
+        labels=dict(x="Right Ascension", y="Declination"),
+        y = decs,
+        x = ras,
+        title = "SAO2000 Sky coverage"
+    )
+    fig.update_layout(
+    autosize = False,
+    width = 800,
+    height = 600)
+    fig.show()
+    plt.offline.plot(fig, filename= f'{os.getcwd()}/sao2000.html')
 
 def getUCACBins():
+    """
+    Retrieves UCAC4 bins from the pickle file.
+    Returns:
+        Numpy array: UCAC4 bins.
+    """
     try:
-        with open("../Plots/ucac_bins", "rb") as f:
+        with open(f"{os.getcwd()}/ucac_bins", "rb") as f:
             unpickled_array = pickle.load(f)
         return unpickled_array
     except Exception as e: 
         print(e)
-        bins_ucac = createUCACBins()
-        return bins_ucac
 
 def plotUCAC():
-    bins_raw = getUCACBins()
-    bins_ucac = []
-    for row in range(len(bins_raw)):
-        bins_ucac.append(bins_raw[row].split(" "))
-    ras, decs = scales([0,360],[-90,90],360,180)
-    a = [list(x) for x in zip(*bins_ucac)]
-    fig = px.imshow(a,
-                    labels=dict(x="Right Ascension", y="Declination"),
-                    y = decs,
-                    x = ras,
-                    title = "UCAC4 Sky Coverage"
-                   )
+    """
+    Plots UCAC4 Sky coverage using the UCAC4 bins.
+    Returns:
+        None
+    """
+    bins = getUCACBins()
+    ras, decs = scales([360, 0],[-90, 90], 360, 180)
+    a = [list(x) for x in zip(*bins)]
+    fig = px.imshow(
+        a,
+        labels = dict(x="Right Ascension", y = "Declination"),
+        y = decs,
+        x = ras,
+        title = "UCAC Sky Coverage"
+    )
     fig.update_layout(
-    autosize=False,
-    width=800,
-    height=600)
+    autosize = False,
+    width = 800,
+    height = 600)
     fig.show()
-    plt.offline.plot(fig, filename='../Plots/ucac4.html')
+    plt.offline.plot(fig, filename = f'{os.getcwd()}/ucac4.html')
 
+def getGSCBins():
+    """
+    Retrieves GSC bins from the pickle file.
+    Returns:
+        Numpy array: GSC bins.
+    """
+    try:
+        with open(f"{os.getcwd()}/gsc240_bins", "rb") as f:
+            unpickled_array = pickle.load(f)
+        return unpickled_array
+    except Exception as e: 
+        print(e)
+        
+def plotGSC():
+    """
+    Plots GSC Sky coverage using the GSC bins.
+    Returns:
+        None
+    """
+    a = getGSCBins()
+    b = [list(x) for x in zip(*a)]
+    ras, decs = scales([360,0],[-90,90],360,180)
+    fig = px.imshow(
+        b,
+        labels=dict(x="Right Ascension", y = "Declination"),
+        y = decs,
+        x = ras,
+        title = "GSC240 Sky coverage"
+    )
+    fig.update_layout(
+    autosize = False,
+    width = 800,
+    height = 600)
+    fig.show()
+    plt.offline.plot(fig, filename = f'{os.getcwd()}/gsc240.html')
+
+    
+def getHIPBins():
+    """
+    Retrieves HIP bins from the pickle file.
+    Returns:
+        Numpy array: HIP bins.
+    """
+    try:
+        with open(f"{os.getcwd()}/hip_bins", "rb") as f:
+            unpickled_array = pickle.load(f)
+        return unpickled_array
+    except Exception as e: 
+        print(e)
+
+def plotHIP():
+    """
+    Plots HIP Sky coverage using the HIP bins.
+    Returns:
+        None
+    """
+    bins = getHIPBins()
+    ras, decs = scales([360, 0],[-90, 90], 360, 180)
+    a = [list(x) for x in zip(*bins)]
+    fig = px.imshow(
+        a,
+        labels = dict(x="Right Ascension", y = "Declination"),
+        y = decs,
+        x = ras,
+        title = "HIP Sky Coverage"
+    )
+    fig.update_layout(
+    autosize = False,
+    width = 800,
+    height = 600)
+    fig.show()
+    plt.offline.plot(fig, filename = f'{os.getcwd()}/hip.html')
+    
+def get2MASSBins():
+    """
+    Retrieves 2MASS bins from the pickle file.
+    Returns:
+        Numpy array: 2MASS bins.
+    """
+    try:
+        with open(f"{os.getcwd()}/2mass_bins", "rb") as f:
+            unpickled_array = pickle.load(f)
+        return unpickled_array
+    except Exception as e: 
+        print(e)
+    
+def plot2MASS():
+    """
+    Plots 2MASS Sky coverage using the 2MASS bins.
+    Returns:
+        None
+    """
+    bins = get2MASSBins()
+    
+    ras, decs = scales([360, 0],[-90, 90], 360, 180)
+    a = [list(x) for x in zip(*bins)]
+    # print(bins)
+    fig = px.imshow(
+        a,
+        labels = dict(x="Right Ascension", y = "Declination"),
+        y = decs,
+        x = ras,
+        title = "2MASS Sky Coverage"
+    )
+    fig.update_layout(
+    autosize = False,
+    width = 800,
+    height = 600)
+    fig.show()
+    plt.offline.plot(fig, filename = f'{os.getcwd()}/2mass.html')
+
+def getGAIABins():
+    """
+    Retrieves GAIA bins from the pickle file
+    Returns:
+        Numpy array: GAIA bins.
+    """
+    try:
+        with open(f"{os.getcwd()}/gaia_bins", "rb") as f:
+            unpickled_array = pickle.load(f)
+        return unpickled_array
+    except Exception as e: 
+        print(e)
+    
+    
+def plotGAIA():
+    """
+    Plots GAIA Sky coverage using the GAIA bins.
+    Returns:
+        None
+    """
+    bins = getGAIABins()
+    
+    ras, decs = scales([360, 0],[-90, 90], 360, 180)
+    a = [list(x) for x in zip(*bins)]
+    fig = px.imshow(
+        a,
+        labels = dict(x="Right Ascension", y = "Declination"),
+        y = decs,
+        x = ras,
+        title = "GAIA Sky Coverage"
+    )
+    fig.update_layout(
+    autosize = False,
+    width = 800,
+    height = 600)
+    fig.show()
+    plt.offline.plot(fig, filename = f'{os.getcwd()}/gaia.html')
+    
 if __name__ == "__main__":
-    # createSAOBins()
-    # createUCACBins()
-    getUCACBins()
-    getSAOBins()
     plotSAO()
     plotUCAC()
+    plotGSC()
+    plotHIP()
+    plot2MASS()
+    plotGAIA()
